4f51f60a4044e8de07417cb3cd70555f43778778
GANESH BASAVOJU <basavojuganesh@gmail.com>
2025-11-18 22:04:11 +0530

    finished

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001", // ✅ Correct Backend URL
+  baseURL: "https://lingstitute-server.vercel.app/", // ✅ Correct Backend URL
   headers: {
     "Content-Type": "application/json",
   },
